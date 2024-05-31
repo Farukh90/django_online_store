9d0a0f6cc467c5a5463b4ac5fe426e193322110b
Farukh <shutdownrunner@bk.ru>
2024-05-31 19:06:14 +0300

    реализован функÑционал приложения каталог согласно ДЗ

---
+++ catalog/utils.py
@@ -0,0 +1,27 @@
+import json
+import os.path
+
+
+def create_contact_dict(name: str, phone: str, message: str) -> dict:
+    contact_data = {
+        'name': name,
+        'phone': phone,
+        'message': message
+    }
+
+    return contact_data
+
+
+def read_JSON_data(file_path: str) -> list:
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+    else:
+        data = []
+        print(f'файл {file_path} не существует')
+    return data
+
+
+def write_JSON_data(file_path: str, data) -> None:
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
