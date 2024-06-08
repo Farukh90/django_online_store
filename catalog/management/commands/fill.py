c79b18e40dabd4fabba90119cf450f46f365e645
Farukh <shutdownrunner@bk.ru>
2024-06-08 04:42:04 +0300

    оптимизации в скрипте филл

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,8 +1,3 @@
-# import os
-# from django.core.management import BaseCommand
-# from django.db import connection
-import os
-from django.core.management import BaseCommand
 import os
 from django.core.management import BaseCommand
 from django.db import connection
@@ -10,8 +5,12 @@ from django.db import connection
 from ...models import Product, Category
 from ...utils import read_JSON_data
 
-category_file_path = os.path.join("catalog", "management", "commands", "data", "category.json")
-products_file_path = os.path.join("catalog", "management", "commands", "data", "product.json")
+category_file_path = os.path.join(
+    "catalog", "management", "commands", "data", "category.json"
+)
+products_file_path = os.path.join(
+    "catalog", "management", "commands", "data", "product.json"
+)
 
 
 class Command(BaseCommand):
@@ -36,22 +35,14 @@ class Command(BaseCommand):
     def load_categories(self):
         """Загружаем в БД данные по категориям"""
         category_list = read_JSON_data(category_file_path)
-        categories = []
         for category_item in category_list:
             category_fields = category_item["fields"]
-            category_obj = Category(id=category_item["pk"], **category_fields)
-            categories.append(category_obj)
-        Category.objects.bulk_create(categories)
+            Category.objects.create(**category_fields)
 
     def load_products(self):
         """Загружаем в БД данные по продуктам"""
         product_list = read_JSON_data(products_file_path)
-        products = []
         for product_item in product_list:
             product_fields = product_item["fields"]
-            category_id = product_fields["category"]
-            category_obj = Category.objects.get(id=category_id)
-            product_fields["category"] = category_obj
-            product_obj = Product(id=product_item["pk"], **product_fields)
-            products.append(product_obj)
-        Product.objects.bulk_create(products)
+            category_id = product_fields.pop("category")
+            Product.objects.create(category_id=category_id, **product_fields)
