9eb521349aced94b9f4c5a5105ad3ee0b37b34e0
Farukh <shutdownrunner@bk.ru>
2024-07-14 16:22:02 +0300

    реализована аутентификация пользователя

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE, help_text="Введите номер телефона")
     country = models.CharField(max_length=150, verbose_name="Страна", help_text="Введите страну проживания")
     avatar = models.ImageField(upload_to="users/avatars/", **NULLABLE, help_text="Загрузите свой аватар")
+    token = models.CharField(max_length=100, verbose_name="Token", **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
