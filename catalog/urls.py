efea10cb6b7e7d5b3d33ec496ffd8239defbfd05
Farukh <shutdownrunner@bk.ru>
2024-07-27 18:11:20 +0300

    настроил рэддис. поправка пакета рэдис в пайтоне

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import (
     ProductDetailView,
@@ -13,7 +15,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="products_list"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_details"),
+    path("product/<int:pk>/", cache_page(60)(ProductDetailView.as_view()), name="product_details"),
     path("product/create", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete", ProductDeleteView.as_view(), name="product_delete"),
